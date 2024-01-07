8be28a415e245e8aba2627ad596014f323877925
imeshme <imeshmadushan927@gmail.com>
2024-01-07 14:08:28 +0530

    Create Apps

---
+++ WindowsFormsApp-GUI Project/WindowsFormsApp-GUI Project/GradiantPanel.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace GradientTemplate
+{
+    public class GradientPanel : Panel
+    {
+        public Color StartColor { get; set; }
+        public Color EndColor { get; set; }
+        public LinearGradientMode GradientMode { get; set; }
+
+        public GradientPanel()
+        {
+            // Set default gradient properties
+            StartColor = Color.FromArgb(255, 128, 128, 255);
+            EndColor = Color.White;
+            GradientMode = LinearGradientMode.Vertical;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            using (LinearGradientBrush brush = new LinearGradientBrush(this.ClientRectangle, StartColor, EndColor, GradientMode))
+            {
+                e.Graphics.FillRectangle(brush, this.ClientRectangle);
+            }
+            base.OnPaint(e);
+        }
+    }
+}
---
+++ WindowsFormsApp-GUI Project/WindowsFormsApp-GUI Project/formLogin.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_GUI_Project
+{
+    public partial class formLogin : Form
+    {
+        public formLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void gradientPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonLogin_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void buttonRegister_Paint(object sender, PaintEventArgs e)
+        {
+           
+        }
+
+        private void buttonRegister_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
