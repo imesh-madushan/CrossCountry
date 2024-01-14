9ad601f79bcdfb20794e68252e2b37be27974feb
imeshme <imeshmadushan927@gmail.com>
2024-01-14 21:14:03 +0530

    dashboard item creation backend done

--- WPFApp-GUI Project/WPFApp-GUI Project/Dashboard.xaml.cs
+++ WPFApp-GUI Project/WPFApp-GUI Project/Dashboard.xaml.cs
@@ -30,96 +30,100 @@ namespace WPFApp_GUI_Project
             //creating items.
             try
             {
-                //creating grid for allocate spaces to items in wrappanel
-                Grid itemGrid = new Grid
+                while (itemCount < 19)
                 {
-                    Name = $"I{itemCount}",
-                    Width = 233,
-                    Height = 190
-                };
+                    //creating grid for allocate spaces to items in wrappanel
+                    Grid itemGrid = new Grid
+                    {
+                        Name = $"I{itemCount}",
+                        Width = 233,
+                        Height = 190
+                    };
 
-                //creating border which has green color
-                Border outlineBorder = new Border
-                {
-                    CornerRadius = new(15),
-                    BorderBrush = new SolidColorBrush(Color.FromArgb(255, 34, 184, 134)),
-                    BorderThickness = new(2),
-                    Margin = new(20, 25, 20, 0)
-                };
+                    //creating border which has green color
+                    Border outlineBorder = new Border
+                    {
+                        CornerRadius = new(15),
+                        BorderBrush = new SolidColorBrush(Color.FromArgb(255, 34, 184, 134)),
+                        BorderThickness = new(2),
+                        Margin = new(20, 25, 20, 0)
+                    };
 
-                //creating item image
-                Image itemImage = new Image
-                {
-                    Source = new BitmapImage(new Uri($"/products/{itemGrid.Name}")),
-                    HorizontalAlignment = HorizontalAlignment.Center,
-                    VerticalAlignment = VerticalAlignment.Top,
-                    RenderTransformOrigin = new(0.5, 0.5),
-                    Width = 151,
-                    Height = 136,
-                    Stretch = Stretch.UniformToFill,
-                    Margin = new(0, 30, 0, 0)
-                };
+                    //creating item image
+                    Image itemImage = new Image
+                    {
+                        Source = new BitmapImage(new Uri($"pack://application:,,,/WPFApp-GUI Project;component/products/{itemGrid.Name}.jpg", UriKind.Absolute)),
+                        HorizontalAlignment = HorizontalAlignment.Center,
+                        VerticalAlignment = VerticalAlignment.Top,
+                        RenderTransformOrigin = new(0.5, 0.5),
+                        Width = 151,
+                        Height = 136,
+                        Stretch = Stretch.UniformToFill,
+                        Margin = new(0, 30, 0, 0)
+                    };
 
-                //creating lable for display price
-                Label priceLable = new Label
-                {
-                    Name = $"lablePrice{itemGrid.Name}",
-                    Content = $"LKR {price}",
-                    Margin = new(33, 34, 102, 126),
-                    FontFamily = new FontFamily("Segoe UI Variable Small Semibold"),
-                    FontSize = 14,
-                };
+                    //creating lable for display price
+                    Label priceLable = new Label
+                    {
+                        Name = $"lablePrice{itemGrid.Name}",
+                        Content = $"LKR {price}",
+                        Margin = new(33, 34, 102, 126),
+                        FontFamily = new FontFamily("Segoe UI Variable Small Semibold"),
+                        FontSize = 14,
+                    };
 
-                //creating lable for display available quantity
-                Label qtyLable = new Label
-                {
-                    Name = $"lableQty{itemGrid.Name}",
-                    Content = $"Qty{quantity}",
-                    Margin = new(144, 32, 17, 127),
-                    FontFamily = new FontFamily("Segoe UI Variable Small Semibold"),
-                    FontSize = 14,
-                };
+                    //creating lable for display available quantity
+                    Label qtyLable = new Label
+                    {
+                        Name = $"lableQty{itemGrid.Name}",
+                        Content = $"Qty {quantity}",
+                        Margin = new(144, 32, 17, 127),
+                        FontFamily = new FontFamily("Segoe UI Variable Small Semibold"),
+                        FontSize = 14,
+                    };
 
-                //creating Buy Now button
-                Button buyNowButton = new Button
-                {
-                    Name = $"buttonBuyNow{itemGrid.Name}",
-                    HorizontalAlignment = HorizontalAlignment.Left,
-                    VerticalAlignment = VerticalAlignment.Top,
-                    Margin = new(36, 151, 0, 0),
-                    Width = 72,
-                    Height = 26,
-                    Style = (Style)FindResource("RoundedButtonBuyStyles"),
-                    FontFamily = new FontFamily("Segoe UI Variable Small Semibold"),
-                    FontSize = 11,
-                    BorderThickness = new Thickness(0,0,0,0)                    
-                };
-                buyNowButton.Click += BuyButton_Click;
-                buyNowButton.MouseEnter += ButtonBuyNow_MouseEnter;
-                buyNowButton.MouseLeave += ButtonBuyNow_MouseLeave;
+                    //creating Buy Now button
+                    Button buyNowButton = new Button
+                    {
+                        Name = $"buttonBuyNow{itemGrid.Name}",
+                        HorizontalAlignment = HorizontalAlignment.Left,
+                        VerticalAlignment = VerticalAlignment.Top,
+                        Margin = new(36, 151, 0, 0),
+                        Width = 72,
+                        Height = 26,
+                        Style = (Style)FindResource("RoundedButtonBuyStyle"),
+                        FontFamily = new FontFamily("Segoe UI Variable Small Semibold"),
+                        FontSize = 11,
+                        BorderThickness = new Thickness(0, 0, 0, 0)
+                    };
+                    buyNowButton.Click += BuyButton_Click;
+                    buyNowButton.MouseEnter += ButtonBuyNow_MouseEnter;
+                    buyNowButton.MouseLeave += ButtonBuyNow_MouseLeave;
 
-                //creating add to cart button
-                Button addToCartButton = new Button
-                {
-                    Height = 25,
-                    Style = (Style)FindResource("PngButtonStyle"),
-                    Margin = new(160, 151, 39, 0),
-                    VerticalAlignment = VerticalAlignment.Top,
-                    BorderBrush = Brushes.White,
-                    Foreground = Brushes.White,
-                    Background = Brushes.White,
-                };
-                addToCartButton.Click += AddToCartButton_Click;
+                    //creating add to cart button
+                    Button addToCartButton = new Button
+                    {
+                        Height = 25,
+                        Style = (Style)FindResource("PngButtonStyle"),
+                        Margin = new(160, 151, 39, 0),
+                        VerticalAlignment = VerticalAlignment.Top,
+                        BorderBrush = Brushes.White,
+                        Foreground = Brushes.White,
+                        Background = Brushes.White,
+                    };
+                    addToCartButton.Click += AddToCartButton_Click;
 
-                //add all abaove created deature to wrappanel
-                wrapPanelItems.Children.Add(itemGrid);
-                wrapPanelItems.Children.Add(outlineBorder);
-                wrapPanelItems.Children.Add(itemImage);
-                wrapPanelItems.Children.Add(priceLable);
-                wrapPanelItems.Children.Add(qtyLable);
-                wrapPanelItems.Children.Add(buyNowButton);
-                wrapPanelItems.Children.Add(addToCartButton);
-                
+                    //add all abaove created deature to wrappanel
+                    wrapPanelItems.Children.Add(itemGrid);
+                    itemGrid.Children.Add(outlineBorder);
+                    itemGrid.Children.Add(itemImage);
+                    itemGrid.Children.Add(priceLable);
+                    itemGrid.Children.Add(qtyLable);
+                    itemGrid.Children.Add(buyNowButton);
+                    itemGrid.Children.Add(addToCartButton);
+
+                    itemCount++; ///remove this, used for testing
+                }
 
             }
             catch (Exception ex)
